118c66ab0fef07cf91fff12e16f95b0c65564ca4
darshanh429 <darshan.hampannavar@in.bosch.com>
2026-04-29 16:47:29 +0000

    Jira automation

---
+++ script.py
@@ -0,0 +1,38 @@
+import os
+import json
+import yaml
+import requests
+from requests.auth import HTTPBasicAuth
+
+with open("jira_data.yml", "r") as f:
+    data = yaml.safe_load(f)
+
+issue_key = data["issue_key"]
+priority = data["priority"]
+
+JIRA_URL = "https://bosch-pmt.atlassian.net"
+EMAIL = os.environ["JIRA_EMAIL"]
+TOKEN = os.environ["JIRA_TOKEN"]
+
+url = f"{JIRA_URL}/rest/api/3/issue/{issue_key}"
+
+payload = {
+    "fields": {
+        "priority": {
+            "name": priority
+        }
+    }
+}
+
+response = requests.put(
+    url,
+    headers={
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    },
+    auth=HTTPBasicAuth(EMAIL, TOKEN),
+    data=json.dumps(payload)
+)
+
+print("Status:", response.status_code)
+print(response.text)
